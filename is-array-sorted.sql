a0b1c6651e5340673abdad90d811f33634d81043
Hellmar Becker <becker@hellmar-becker.de>
2025-01-03 10:37:48 +0100

    Update is-array-sorted.sql

--- is-array-sorted.sql
+++ is-array-sorted.sql
@@ -6,3 +6,22 @@ SELECT arrayFold(
     arrayPopFront(data),
     [(CAST(data[1], 'Int64'), 1::UInt8)]
 )[-1].2 AS ordered;
+
+
+CREATE OR REPLACE FUNCTION isMonotonous AS (x) ->
+    arrayFold(
+        (acc, x) -> ( x::Int32, acc.2 AND x >= acc.1 ),
+        arrayPopFront(x),
+        (x[1]::Int32, 1::UInt8)
+    ).2;
+
+SELECT isMonotonous([ 1, 4, 6, 40 ]);
+
+CREATE OR REPLACE FUNCTION eliminateBacksteps AS (x) ->
+    arrayFold(
+        (acc, x) -> ( if(x >= acc.1[-1], arrayPushBack(acc.1, x), acc.1), x >= acc.1[-1] ),
+        arrayPopFront(x),
+        ([ x[1]::Int32 ], 1::UInt8)
+    );
+
+SELECT eliminateBacksteps([ 3, 4, 6, 4, 7, 2, 4, 10 ]);
